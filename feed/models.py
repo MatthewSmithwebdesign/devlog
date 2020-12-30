b70a66ece05a1c4a1d789319c02bc7375cbd6aff
Matthew Smith <matthewsmithwebdesign@gmail.com>
2020-12-29 16:46:34 -0800

    worked on view set up static files separated inline css to standalone files

--- feed/models.py
+++ feed/models.py
@@ -17,9 +17,10 @@ class BlogPost(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-
+    image = models.ImageField(upload_to='images')
 class Meta:
     ordering = ['-created_on']
 
 def __str__(self):
         return self.title
+
