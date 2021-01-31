bd5928b8f94292306bb42506c1e3766a01f5ca63
Matthew Smith <matthewsmithwebdesign@gmail.com>
2021-01-31 13:03:18 -0800

    rewrote comment model

--- comment/views.py
+++ comment/views.py
@@ -1,40 +1,30 @@
+from django.utils import timezone
 from django.views.generic.detail import DetailView
 from django.shortcuts import render, get_object_or_404
 from feed.models import BlogPost
 from .forms import CommentForm
+from .models import Comment
 
 # Create your views here.
 
 
 class PostDetail(DetailView):
+    model = BlogPost
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        comments_connected = Comment.objects.filter(
+            blogpost_connected=self.get_object()
+        ).order_by("date_posted")
+        data["comments"] = comments_connected
+        data["comment_form"] = CommentForm()
+        return data
+
     def post(self, request, *args, **kwargs):
-        template_name = "post_detail.html"
-        http_method_names = ["get", "post"]
-        comments = post.comments.filter(active=True)
-        # post = get_object_or_404(BlogPost, slug=slug)
-       
-        # new_COMMENT = None
-        # Posted Comment
-        if request.method == "POST":
-            comment_form = CommentForm(data=request.POST)
-            if comment_form.is_valid():
-                # unsaved created comment
-                new_comment = comment_form.save(commit=False)
-                # attaching comment to posts
-                new_comment.post = post
-                # save to the database
-                new_comment.save()
-
-            else:
-                comment_form = CommentForm()
-
-                return render(
-                    request,
-                    template_name,
-                    {
-                        "post": post,
-                        "comments": comments,
-                        "new_comment": new_comment,
-                        "comment_form": comment_form,
-                    },
-                )
+        new_comment = Comment(
+            body=request.POST.get("body"),
+            name=request.POST.get("name"),
+            post_connected=self.get_object(),
+        )
+        new_comment.save()
+        return self.get(self, request, *args, **kwargs)
